dc65e8131fb85bd76ec2e48b170db81dc885ad8c
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-01-25 22:22:38 +0800

    fixed resources issue

--- IOOP_assignment/Properties/Resources.Designer.cs
+++ IOOP_assignment/Properties/Resources.Designer.cs
@@ -73,16 +73,6 @@ namespace IOOP_assignment.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap add {
-            get {
-                object obj = ResourceManager.GetObject("add", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap add1 {
             get {
                 object obj = ResourceManager.GetObject("add1", resourceCulture);
