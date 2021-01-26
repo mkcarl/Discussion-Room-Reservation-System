e029acf13ec450b99bea23f19f7a3f1f56035541
TAN SHENG JEH <sjt2683@gmail.com>
2021-01-27 01:50:54 +0800

    Pending Student Requests

--- IOOP_assignment/PendingStudentRequests.Designer.cs
+++ IOOP_assignment/PendingStudentRequests.Designer.cs
@@ -29,19 +29,113 @@ namespace IOOP_assignment
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Day = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Room = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Number_Of_Students = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.lblPendingStudentReq = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Time,
+            this.Day,
+            this.Room,
+            this.Number_Of_Students,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(43, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1154, 500);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.MinimumWidth = 6;
+            this.Time.Name = "Time";
+            this.Time.Width = 125;
+            // 
+            // Day
+            // 
+            this.Day.HeaderText = "Day";
+            this.Day.MinimumWidth = 6;
+            this.Day.Name = "Day";
+            this.Day.Width = 125;
+            // 
+            // Room
+            // 
+            this.Room.HeaderText = "Room";
+            this.Room.MinimumWidth = 6;
+            this.Room.Name = "Room";
+            this.Room.Width = 125;
+            // 
+            // Number_Of_Students
+            // 
+            this.Number_Of_Students.HeaderText = "Number_Of_Students";
+            this.Number_Of_Students.MinimumWidth = 6;
+            this.Number_Of_Students.Name = "Number_Of_Students";
+            this.Number_Of_Students.Width = 125;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 125;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(521, 597);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(123, 51);
+            this.btnConfirm.TabIndex = 1;
+            this.btnConfirm.Text = "Confirm";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // lblPendingStudentReq
+            // 
+            this.lblPendingStudentReq.AutoSize = true;
+            this.lblPendingStudentReq.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPendingStudentReq.Location = new System.Drawing.Point(467, 9);
+            this.lblPendingStudentReq.Name = "lblPendingStudentReq";
+            this.lblPendingStudentReq.Size = new System.Drawing.Size(244, 25);
+            this.lblPendingStudentReq.TabIndex = 2;
+            this.lblPendingStudentReq.Text = "Pending Student Requests";
             // 
             // PendingStudentRequests
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.lblPendingStudentReq);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "PendingStudentRequests";
             this.Text = "PendingStudentRequests";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Day;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Room;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number_Of_Students;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label lblPendingStudentReq;
     }
 }
