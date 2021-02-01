577d00da685f92772cd954b0b31054e2e6941197
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-02-01 22:36:13 +0800

    Added resizable MonthCalendar control

---
+++ IOOP_assignment/CustomControl1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace IOOP_assignment
+{
+    public partial class CustomControl1 : Control
+    {
+        public CustomControl1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        
+    }
+    public class MyMonthCalendar : MonthCalendar
+    {
+        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string pszSubIdList);
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            SetWindowTheme(Handle, string.Empty, string.Empty);
+            base.OnHandleCreated(e);
+        }
+    }
+}
