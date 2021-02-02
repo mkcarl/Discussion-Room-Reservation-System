8f7734fe5e5bebd8e56f4b4e7eb57a4baa5178c3
mkcarl <53103933+mkcarl@users.noreply.github.com>
2021-02-02 22:59:01 +0800

    added error message for wrong input in student ID txt box

--- IOOP_assignment/Login.cs
+++ IOOP_assignment/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace IOOP_assignment
 {
@@ -15,6 +16,47 @@ namespace IOOP_assignment
         public formLogin()
         {
             InitializeComponent();
+        }
+
+        private void txtStudentID_Login_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtStudentID_Login_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*pseudocode
+
+           if input is number or backspace, accept input,
+           else reject, then tell that you only can insert numbers
+
+            */
+
+
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) // if the input is not number or a control like backspace
+            {
+                showErrorHint("Only accept number inputs", txtStudentID_Login); // show the hint 
+                e.Handled = true; // stop the input
+            }
+            else if (txtStudentID_Login.Text.Length>=6) // if the input is more than 6 characters
+            {
+                if (!char.IsControl(e.KeyChar)) { 
+                    e.Handled = true;
+                    showErrorHint("Student ID is only 6 characters long.", txtStudentID_Login);
+                }
+                
+            }
+        }
+
+        // TODO: move showHint() to Controller class. 
+        private void showErrorHint(string message, Control control)
+        {
+            SystemSounds.Asterisk.Play();
+            ToolTip tt = new ToolTip();
+            tt.IsBalloon = true;
+            tt.Show(message, control, 150, -30, 500);
+
+            // https://stackoverflow.com/questions/14695357/show-tooltip-on-textbox-entry
         }
     }
 }
