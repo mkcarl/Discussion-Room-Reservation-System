6deab3425e4336e57ab843ffaa63610eaf1a80c6
HOR SHEN HAU <TP061524@mail.apu.edu.my>
2021-02-16 01:58:29 +0800

    corressponded number of people combo box to radio buttons

--- IOOP_assignment/Reserve Room.cs
+++ IOOP_assignment/Reserve Room.cs
@@ -29,19 +29,47 @@ namespace IOOP_assignment
 
         private void comboPeopleReserve_SelectedIndexChanged(object sender, EventArgs e)
         {
-            comboPeopleReserve.ToString.Text();
-
-            if (comboPeopleReserve < 8)
+            if (int.Parse(comboPeopleReserve.SelectedItem.ToString()) > 8)
             {
-
+                radAmberReseve.Enabled = true;
+                radBlackThornReserve.Enabled = false;
+                radCedarReserve.Enabled = false;
+                radDaphneReserve.Enabled = false;
             }
-            
+
+            else if ((int.Parse(comboPeopleReserve.SelectedItem.ToString()) < 8) && (int.Parse(comboPeopleReserve.SelectedItem.ToString()) > 4))
+            {
+                radAmberReseve.Enabled = true;
+                radBlackThornReserve.Enabled = true;
+                radCedarReserve.Enabled = false;
+                radDaphneReserve.Enabled = false;
+            }
+            else if ((int.Parse(comboPeopleReserve.SelectedItem.ToString()) < 4) && (int.Parse(comboPeopleReserve.SelectedItem.ToString()) > 2))
+            {
+                radAmberReseve.Enabled = true;
+                radBlackThornReserve.Enabled = true;
+                radCedarReserve.Enabled = true;
+                radDaphneReserve.Enabled = false;
+            }
+            else if ((int.Parse(comboPeopleReserve.SelectedItem.ToString()) < 2) && (int.Parse(comboPeopleReserve.SelectedItem.ToString()) > 0))
+            {
+                radAmberReseve.Enabled = false;
+                radBlackThornReserve.Enabled = true;
+                radCedarReserve.Enabled = true;
+                radDaphneReserve.Enabled = true;
+            }
         }
 
         private void monthCalendarReserve_DateChanged(object sender, DateRangeEventArgs e)
         {
             monthCalendarReserve.MinDate = DateTime.Now.AddDays(2);
             monthCalendarReserve.MaxDate = DateTime.Now.AddDays(7);
+
+        }
+
+        private void FormReserve_Load(object sender, EventArgs e)
+        {
+            
 
         }
     }
